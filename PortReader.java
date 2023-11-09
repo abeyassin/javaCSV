1bf59c79d2579db33979fea74d23ece989edc3e3
Ibrahim Yassin <iay5015@psu.edu>
2023-11-08 22:56:28 -0500

    Part of the redis project.

---
+++ PortReader.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PortReader {
+    public static void main(String[] args) {
+        String pathToCsv = "Yassin PortScan JAVA.csv"; // replace with your CSV file path
+        String line;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(pathToCsv))) {
+
+            while ((line = br.readLine()) != null) {
+                // Use comma as separator
+                String[] columns = line.split(",");
+
+                // Print the content on the console
+                //for (String column : columns) {
+                    System.out.print(columns [10]+ " " + columns[4]);
+                //}
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
